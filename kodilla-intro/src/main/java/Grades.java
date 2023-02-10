dadfcf10d2d6f80ef427d1f41493718ca9a27b7b
Lidia Nowakowska <l.e.nowakowska@gmail.com>
2023-02-10 09:29:47 +0100

    Exercise 2.4 v1

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,39 @@
+public class Grades {
+    public static void main(String[] args) {
+        Grades grades = new Grades();
+        //adding example grades:
+        grades.add(4); // at the end size value 0 + 1, index 0
+        grades.add(2); // at the end size value 1 + 1, index 1
+        grades.add(6); // at the end size value 2 + 1, index 2
+
+        //methods last grade and average grade:
+        grades.lastGrade();
+        grades.averageGrade();
+    }
+    private int[] grades; // 1) czemu private przed int, w klasie notebook zmienne były deklarowane bez private
+    private int size;
+    public Grades(){
+        this.grades = new int[10];
+        this.size = 0;
+    }
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+    public void lastGrade(){
+        int lastGrade = grades[this.size - 1];
+        System.out.println("The last grade added is " + lastGrade);
+    }
+    public void averageGrade(){
+        int numberOfGrades = this.size;
+        int total = 0;
+        for (int i = 0; i <= numberOfGrades; i++) {
+            total += this.grades[i];
+        }
+        int averageGrade = total/this.size;
+        System.out.println("The average grade is " + averageGrade);
+    }
+}
