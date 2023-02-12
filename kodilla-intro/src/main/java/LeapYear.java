da633a9a287202bf8cb2d98427b2268a39c3d33a
Lidia Nowakowska <l.e.nowakowska@gmail.com>
2023-02-12 18:08:42 +0100

    LeapYear exercise, added condition for 238 BC

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,18 +1,21 @@
 public class LeapYear {
     public static void main(String[] args) {
-        isItLeapYear(2024);
+        isItLeapYear(2023);
     }
     private static void isItLeapYear (int year){
         int moduloDividingByFour = year%4;
         int moduloDividingByHundred = year%100;
         int moduloDividingByFourHundred = year%400;
 
-        if (moduloDividingByFour!=0){
-            System.out.println("This is not Leap Year (second method");
+        if (year < -238){
+            System.out.println("You have to insert year which is after 232 BC");
+        }
+        else if (moduloDividingByFour!=0){
+            System.out.println("This is not Leap Year");
         } else if (moduloDividingByHundred!=0){
-            System.out.println("This is Leap Year (second method)");
+            System.out.println("This is Leap Year");
         } else if (moduloDividingByFourHundred!=0){
-            System.out.println("This is not Leap Year (second method)");
+            System.out.println("This is not Leap Year");
         } else {
             System.out.println("This is Leap Year");
         }
