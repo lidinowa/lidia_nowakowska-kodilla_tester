de92018e757f9c67869676a56681f20fd8e74078
Lidia Nowakowska <l.e.nowakowska@gmail.com>
2023-02-20 15:31:16 +0100

    Commit for exercise 2.5 v.2 added get methods to RandomNumbers

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,29 +1,75 @@
 import java.util.Random;
 
 public class RandomNumbers {
-    public static void main(String[] args) {
-        Random random = new Random();
-        int sumOfNumbers = 0;
-        int maxNumber = 0;
-        int minNumber = 30;
-        while (sumOfNumbers < 5000) {
-            int newNumber = random.nextInt(31);
-            sumOfNumbers += newNumber;
-            System.out.println("New number is " + newNumber + " and current sum is " + sumOfNumbers);
+    Random random = new Random();
+    int min;
+    int max;
 
-            if (newNumber < minNumber) {
-                minNumber = newNumber;
+    public void drawRandom() {
+        int sum = 0;
+        this.min = 30;
+        this.max = 0;
+        while (sum < 500) {
+            int newNumber = random.nextInt(31);
+            sum += newNumber;
+            System.out.println("New number is " + newNumber + " and current sum is " + sum);
+            if (newNumber < min) {
+                min = newNumber;
                 //   System.out.println("This time min. number is " + minNumber);
             }
-            if (newNumber > maxNumber) {
-                maxNumber = newNumber;
+            if (newNumber > max) {
+                max = newNumber;
                 //  System.out.println("This time max. number is " + maxNumber);
             }
         }
-        System.out.println("Min. number obtained is " + minNumber);
-        System.out.println("Max. number obtained is " + maxNumber);
+    }
+
+    public void getMinNumber() {
+        System.out.println("Min number is " + min);
+    }
+
+    public void getMaxNumber() {
+        System.out.println("Max number is " + max);
+    }
+
+    public static void main(String[] args) {
+        RandomNumbers randomNumbers = new RandomNumbers();
+        randomNumbers.drawRandom();
+        randomNumbers.getMinNumber();
+        randomNumbers.getMaxNumber();
     }
 }
 
+//************ version without methods get:
 
-
+//import java.util.Random;
+//
+//public class RandomNumbers {
+//    public static void main(String[] args) {
+//
+//        Random random = new Random();
+//        int sumOfNumbers = 0;
+//        int maxNumber = 0;
+//        int minNumber = 30;
+//
+//
+//        while (sumOfNumbers < 5000) {
+//            int newNumber = random.nextInt(31);
+//            sumOfNumbers += newNumber;
+//            System.out.println("New number is " + newNumber + " and current sum is " + sumOfNumbers);
+//
+//            if (newNumber < minNumber) {
+//                minNumber = newNumber;
+//                //   System.out.println("This time min. number is " + minNumber);
+//            }
+//            if (newNumber > maxNumber) {
+//                maxNumber = newNumber;
+//                //  System.out.println("This time max. number is " + maxNumber);
+//            }
+//        }
+//        System.out.println("Min. number obtained is " + minNumber);
+//        System.out.println("Max. number obtained is " + maxNumber);
+//
+//    }
+//
+//}
