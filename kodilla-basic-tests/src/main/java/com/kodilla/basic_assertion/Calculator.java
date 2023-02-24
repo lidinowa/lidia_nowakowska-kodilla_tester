eccbe121bcd4c6164abd88bcbce0b33f78a42af2
Lidia Nowakowska <l.e.nowakowska@gmail.com>
2023-02-24 17:35:13 +0100

    Exercise 3.5 v1

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,24 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int toSquare(int a) {
+        return a * a;
+    }
+
+    public double subtractForDouble(double a, double b) {
+        return a - b;
+    }
+
+    public double toSquareForDouble(double a) {
+        return a * a;
+    }
+}
