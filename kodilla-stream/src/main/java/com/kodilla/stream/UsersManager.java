0bb0b751fa2bfd5b47c619ff198391186ee121d9
Lidia Nowakowska <l.e.nowakowska@gmail.com>
2023-03-30 15:37:22 +0200

    Exercise 6.1 v1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -0,0 +1,65 @@
+package com.kodilla.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class UsersManager {
+    public static void main(String[] args) {
+
+        List<String> chemistGroupUsernames = filterChemistGroupUsernames();
+        System.out.println(chemistGroupUsernames);
+        System.out.println(filterUsersOlderThan(40));
+        System.out.println(whatIsAgeOfUsersFromGroup("Chemists"));
+        System.out.println(filterUsersWithManagerRole());
+    }
+
+    public static String getUserName(User user) {
+        return user.getUsername();
+    }
+
+    private static void processUsersStream() {
+        UsersRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getGroup().equals("Chemists"))
+                // .map(user -> user.getUsername())
+                .map(UsersManager::getUserName)
+                // .forEach(username -> System.out.println(username));
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> filterChemistGroupUsernames() {
+        List<String> usernames = UsersRepository.getUsersList()   // [1]
+                .stream()
+                .filter(user -> user.getGroup().equals("Chemists"))
+                .map(UsersManager::getUserName)
+                .collect(Collectors.toList());                      // [2]
+        return usernames;
+    }
+
+    public static List<String> filterUsersOlderThan(int age) {
+        List<String> usersOlderThan = UsersRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getAge() > age)
+                .map(UsersManager::getUserName)
+                .collect(Collectors.toList());
+        return usersOlderThan;
+    }
+
+    public static List<Integer> whatIsAgeOfUsersFromGroup(String groupName) {
+        List<Integer> ageOfUser = UsersRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getGroup().equals(groupName))
+                .map(user -> user.getAge())
+                .collect(Collectors.toList());
+        return ageOfUser;
+    }
+
+    public static List<User> filterUsersWithManagerRole() {
+        List<User> managerList = UsersRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getGroup().equals("Manager"))
+                .collect(Collectors.toList());
+        return managerList;
+    }
+}
