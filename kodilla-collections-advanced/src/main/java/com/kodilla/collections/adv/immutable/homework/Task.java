2ccd2e8a09b1a825970f9f37c911719d22de9900
Lidia Nowakowska <l.e.nowakowska@gmail.com>
2023-03-31 17:29:36 +0200

    Exercise 5.3 v1

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -0,0 +1,11 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class Task {
+    protected String title;
+    protected int duration;
+
+    public Task(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+}
