27c0d82a90cab6e80787d954d97690cb1ea21bc6
Lidia Nowakowska <l.e.nowakowska@gmail.com>
2023-04-11 14:34:25 +0200

    Exercise 2.3 v3

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,22 +1,40 @@
 public class FirstClass {
     public static void main(String[] args) {
-
-        System.out.println("First laptop:");
         Notebook notebook = new Notebook(600, 1000, 2020);
-        notebook.checkPrice();
-        notebook.checkWeight();
-        notebook.isALaptopGoodOffer();
-
-        System.out.println("Second laptop:");
         Notebook heavyNotebook = new Notebook(2000, 1500, 2023);
-        heavyNotebook.checkPrice();
-        heavyNotebook.checkWeight();
-        heavyNotebook.isALaptopGoodOffer();
-
-        System.out.println("Third laptop:");
         Notebook oldNotebook = new Notebook(1600, 500, 2015);
-        oldNotebook.checkPrice();
-        oldNotebook.checkWeight();
-        oldNotebook.isALaptopGoodOffer();
+
+        displayNotebookParameters(notebook);
+        displayNotebookParameters(heavyNotebook);
+        displayNotebookParameters(oldNotebook);
+    }
+
+    public static void displayNotebookParameters(Notebook exampleNotebook) {
+        exampleNotebook.checkPrice();
+        exampleNotebook.checkWeight();
+        exampleNotebook.isALaptopGoodOffer();
     }
 }
+
+//public class FirstClass {
+//    public static void main(String[] args) {
+//
+//        System.out.println("First laptop:");
+//        Notebook notebook = new Notebook(600, 1000, 2020);
+//        notebook.checkPrice();
+//        notebook.checkWeight();
+//        notebook.isALaptopGoodOffer();
+//
+//        System.out.println("Second laptop:");
+//        Notebook heavyNotebook = new Notebook(2000, 1500, 2023);
+//        heavyNotebook.checkPrice();
+//        heavyNotebook.checkWeight();
+//        heavyNotebook.isALaptopGoodOffer();
+//
+//        System.out.println("Third laptop:");
+//        Notebook oldNotebook = new Notebook(1600, 500, 2015);
+//        oldNotebook.checkPrice();
+//        oldNotebook.checkWeight();
+//        oldNotebook.isALaptopGoodOffer();
+//    }
+//}
